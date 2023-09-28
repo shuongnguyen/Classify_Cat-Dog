3e2022f13775501d61059b71e089423bd858bc71
shuongnguyen <shuongnguyen.work@gmail.com>
2023-09-28 15:50:06 +0700

    error at API key, too many request

--- api_test.py
+++ api_test.py
@@ -2,8 +2,9 @@ import logging
 import tornado.httpclient
 import json
 import asyncio
+import aiohttp
 import tornado.web
-import time
+
 
 class Logger:
     def __init__(self, filename=None, filemode='a', level=logging.INFO):
@@ -76,30 +77,12 @@ class Connector:
         http_client = tornado.httpclient.AsyncHTTPClient()
         request = tornado.httpclient.HTTPRequest(url, method=method, headers=headers, body=body, validate_cert=False)
         return await http_client.fetch(request)
-
-async def make_post_requests(connector, num_requests):
-    payload = "source_language=en&target_language=fr&text=What%20is%20your%20name%3F"
-
-    results = []
-
-    # Use a semaphore to control concurrency
-    semaphore = asyncio.Semaphore(10)  # Adjust the concurrency limit as needed
-
-    async def do_post_request(i):
-        async with semaphore:
-            try:
-                response = await connector.make_post_request("/translate", payload)
-                results.append((i, response))
-            except Exception as e:
-                results.append((i, {'error': str(e)}))
-
-    # Create and gather tasks
-    tasks = [do_post_request(i) for i in range(num_requests)]
-    await asyncio.gather(*tasks)
-
-    return results
+    
 
 
+async def fetch_url(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 async def main():
     logger = Logger(filename='mylog.log', level=logging.DEBUG)
@@ -107,7 +90,7 @@ async def main():
         'url': "https://text-translator2.p.rapidapi.com",
         'headers': {
             "content-type": "application/x-www-form-urlencoded",
-            "X-RapidAPI-Key": "0b4f84a007msh88a4bc1868d6a98p1fdf0ejsnbb2330d895e4",
+            "X-RapidAPI-Key": "e2c5743a34msh9da1b038b177d2ap1b2f54jsn8293aa5e1844",
             "X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
         }
     }
@@ -116,22 +99,24 @@ async def main():
     
 
     #GET request
-    get_response = await connector.make_get_request("/getLanguages")
-    logger.info(f"GET Response: {get_response}")
+    # get_response = await connector.make_get_request("/getLanguages")
+    # logger.info(f"GET Response: {get_response}")
     
     # payload = "source_language=en&target_language=fr&text=What%20is%20your%20name%3F"
 
-    #POST request with a request body
+    # #POST request with a request body
     
     # post_response = await connector.make_post_request("/translate", payload)
     # logger.info(f"POST Response: {post_response}")
     
-    num_requests = 500
-    post_responses = await make_post_requests(connector, num_requests)
-    for i, response in enumerate(post_responses):
-        logger.info(f"POST Response {response[0] + 1}: {response[1]}")
-    
+    urls = ["https://text-translator2.p.rapidapi.com"] * 5 
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        tasks = [fetch_url(session, url) for url in urls]
 
+        # Use asyncio.gather to run all tasks concurrently
+        responses = await asyncio.gather(*tasks)
+        logger.info(f"POST Response: {responses}")
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
